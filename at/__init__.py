9ba741a4b98b61d700548321c8668fe7187bd6b5
mebao <ke.v.pham@vntrip.vn>
2016-11-02 15:32:23 +0700

    Update

--- at/__init__.py
+++ at/__init__.py
@@ -2,7 +2,7 @@
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
-from flask import Flask, Blueprint, request, render_template, url_for, send_from_directory
+from flask import Flask, Blueprint, request, render_template, url_for, send_from_directory, redirect
 import logging
 import datetime
 import requests
@@ -60,7 +60,7 @@ def index():
 
                     return render_template('index.html', form=form)
                 else:
-                    return render_template('thankyou.html')
+                    return redirect('/thankyou.html')
 
             form.email.errors.append("Invalid data!")
             return render_template('index.html', form=form)
@@ -71,3 +71,6 @@ def index():
     return render_template('index.html', form=form)
 
 
+@app.route("/thankyou.html", methods=['POST', 'GET'])
+def thankyou():
+    return render_template('thankyou.html')
